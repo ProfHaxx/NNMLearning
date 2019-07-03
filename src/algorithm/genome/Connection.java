6f26314292eebd9234b9e6cb2a1f0ae11bda0a82
Madcap3000 <roman.chernikov@madcap.one>
2019-07-03 14:16:05 -0500

    Connections and Genome updates

--- src/algorithm/genome/Connection.java
+++ src/algorithm/genome/Connection.java
@@ -1,4 +1,37 @@
 package algorithm.genome;
 
-public class Connections {
+public class Connection {
+    private int InputNodeID, OutputNodeID;
+    private double weight;
+
+    public Connection(double weight, int InputNodeID, int OutputNodeID){
+        this.InputNodeID = InputNodeID;
+        this.OutputNodeID = OutputNodeID;
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public int getInputNodeID() {
+        return InputNodeID;
+    }
+
+    public int getOutputNodeID() {
+        return OutputNodeID;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public Connection copy(){
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "InputNodeID: " + InputNodeID + "  OutputNodeID: " + OutputNodeID + "  Weight: " + weight;
+    }
 }
