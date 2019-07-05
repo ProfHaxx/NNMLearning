348f380b0c3d760b7d2371a15e68e5bb42ed53da
Madcap3000 <roman.chernikov@madcap.one>
2019-07-05 10:36:14 -0500

    ChaoticCyclicGenome. Version.1: Gird of the following Work

---
+++ src/algorithm/genome/ChaoticCyclicGenome.java
@@ -0,0 +1,63 @@
+package algorithm.genome;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ChaoticCyclicGenome extends AbstractGenome {
+
+    //both constructors
+    //the second one may be not used at all and only will be useful in recreating the save Genome
+
+    public ChaoticCyclicGenome(Node[] InputNodes, Node[] OutputNodes){
+        super(InputNodes, OutputNodes);
+    }
+
+    public ChaoticCyclicGenome(List<Node> HiddenNodes, List<Connection> connections, Node[] InputNodes, Node[] OutputNodes){
+        super(HiddenNodes, connections, InputNodes, OutputNodes);
+    }
+
+    //private add functions in order to change Genome structure during it's mutations
+
+    private void addHiddenNodes(Node... newHiddenNodes){
+        super.getHiddenNodes().addAll(Arrays.asList(newHiddenNodes));
+    }
+
+    private void addConnections(Connection... newConnections){
+        super.getConnections().addAll(Arrays.asList(newConnections));
+    }
+
+    //get functions
+
+    @Override
+    public List<Connection> getConnections() {
+        return super.getConnections();
+    }
+
+    @Override
+    public Node[] getInputNodes() {
+        return super.getInputNodes();
+    }
+
+    @Override
+    public List<Node> getHiddenNodes() {
+        return super.getHiddenNodes();
+    }
+
+    @Override
+    public Node[] getOutputNodes() {
+        return super.getOutputNodes();
+    }
+
+    //count function is used to find out the number this Genome produces at certain point of time
+
+    @Override
+    public void count() {
+
+    }
+
+    @Override
+    public Genome copy() {
+        return null;
+    }
+
+}
